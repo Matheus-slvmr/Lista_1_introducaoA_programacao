6a47907db3d176530c60ccf4c20b691a913c53e6
Matheus-slvmr <135768933+Matheus-slvmr@users.noreply.github.com>
2026-03-23 20:48:13 -0300

    Add files via upload

---
+++ GO-UFG/GO-UFG/Sala de aula/anotaçoes.go
@@ -0,0 +1,65 @@
+package main
+/*
+pacotes ==
+
+fmt dados de entrada e saida
+math funcoes matematicas
+strconv conversoes
+strings manipulacao de string
+io funcoes basicas de entrada e saida
+net/http implententaçao de clientes e servidores http
+
+dados--->
+
+int64	
+float64
+String
+Bool
+var-->
+
+var xxx ttt
+xxxx = yyy
+
+var(
+xxxx ttt = yyy
+)
+
+operadores de atribuiçao
+
+x = y
+x := y ---> var declarada
+x += y adiciona e atrubui
+x -= y
+x *= y
+x/=y
+x++
+++x
+x--atribui e subtrai
+--x subtrai e atribui
+
+operadores aritimeticos
++
+-
+*
+/
+% resto da divisao 
+++ incremento
+-- decremento
+
+
+
+
+
+
+
+
+
+*/
+
+import "fmt"
+
+func main()  {
+	var nome String
+	fmt.Println("hello")
+	fmt.scan(&)
+}

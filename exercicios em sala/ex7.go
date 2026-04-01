6ec50f85910661fa0f7a8f2593b5d587d062a358
Matheus-slvmr <135768933+Matheus-slvmr@users.noreply.github.com>
2026-04-01 20:44:56 -0300

    7.0

--- exercicios em sala/ex7.go
+++ exercicios em sala/ex7.go
@@ -1,3 +1,17 @@
-/* Escreva uma função em GO chamada Fatorial que retorne o fatorial de um número passado como parâmetro. */
 package main
 import f "fmt"
+func main() {
+   // Declaração e leitura dos números
+   n:= 0
+   f.Print("Digite um número inteiro: ")
+   f.Scan(&n)
+   // Chamada à função fatorial
+   fatorial := fatorial(n)
+   f.Printf("O fatorial de %d é %d", n, fatorial)
+}
+func fatorial(numero int) int {
+   if numero == 0 || numero == 1 {
+	  return 1
+   }
+   return numero * fatorial (numero - 1)
+}

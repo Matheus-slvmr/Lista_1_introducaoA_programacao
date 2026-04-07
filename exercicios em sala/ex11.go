621ccbff99094f2dd117e4dc4b9aa686d9e60fbb
Matheus-slvmr <135768933+Matheus-slvmr@users.noreply.github.com>
2026-04-06 23:08:05 -0300

    2.00

--- exercicios em sala/ex11.go
+++ exercicios em sala/ex11.go
@@ -12,15 +12,15 @@ func main() {
 	var pessoas []Pessoa
 	for {
 		var p Pessoa
-		fmt.Print("Digite o nome da pessoa (ou 'FIM' para encerrar): ")
+		fmt.Print("Digite o nome da pessoa (ou 'fim' para encerrar): ")
 		fmt.Scanln(&p.nome)
-		if p.nome == "FIM" {
+		if p.nome == "fim" {
 			break
 		}
 		fmt.Print("Digite a altura da pessoa: ")
 		fmt.Scanln(&p.altura)
 		p.peso = 72.7*p.altura - 58.0
-		pessoas = append(pessoas, p)
+		pessoas = append(pessoas, p) 
 	}
 	fmt.Println("Dados das pessoas:")
 	for _, pessoa := range pessoas {
